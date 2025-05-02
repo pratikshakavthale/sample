0c118ee3a37250d1736e6f0edfb4e3db33aee5e5
pratikshakavthale <pratikshakavthale.aissmscoe@gmail.co>
2025-05-02 12:42:45 +0530

    second

--- src/swdev.js
+++ src/swdev.js
@@ -1,19 +1,19 @@
 export default function swDev() {
   // Convert base64 VAPID key to Uint8Array
-  function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-      .replace(/-/g, '+')
-      .replace(/_/g, '/');
+  // function urlBase64ToUint8Array(base64String) {
+  //   const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  //   const base64 = (base64String + padding)
+  //     .replace(/-/g, '+')
+  //     .replace(/_/g, '/');
 
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
+  //   const rawData = window.atob(base64);
+  //   const outputArray = new Uint8Array(rawData.length);
 
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-  }
+  //   for (let i = 0; i < rawData.length; ++i) {
+  //     outputArray[i] = rawData.charCodeAt(i);
+  //   }
+  //   return outputArray;
+  // }
 
   // function determineAppServerKey() {
   //   const vapidPublicKey =
