bb9840207cb57b768295e96a1ce673571684ff3e
pratikshakavthale <pratikshakavthale.aissmscoe@gmail.co>
2025-05-02 12:51:01 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Home } from './Home';
 import { About } from './about';
 import { Users } from './user';
@@ -9,27 +9,29 @@ import { InstallPrompt } from './InstallPromptButton';
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
-
   return (
-    <div className="App">
-     <InstallPrompt />
-      <Router>
-        <Navbar bg="primary" variant="dark">
-        
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/users">Users</Nav.Link>
-          </Nav>
+    <Router>
+      <div className="App">
+        <InstallPrompt />
+        <Navbar bg="primary" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/users">Users</Nav.Link>
+            </Nav>
+          </Container>
         </Navbar>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-        </Routes>
-      </Router>
-    </div>
+        <Container className="mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/users" element={<Users />} />
+          </Routes>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
